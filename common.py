25fa28e65999112ba661abbd483f0b8c80dc6402
zjucxh <chen.xianghui@hotmail.com>
2021-05-25 15:40:23 +0800

    locically right

--- common.py
+++ common.py
@@ -1,14 +1,30 @@
 import numpy as np 
 import cv2 as cv 
 
-def project3dpt(cameraMatrix, rvec, tvec, pt3d):
-    rotm, _ = cv.Rodrigues(rvec)
-    pt2d = []
-    for e in pt3d:
-        # image point = K*(R * X + t) * alpha
-        pj = np.dot(cameraMatrix, np.dot(rotm,e)+tvec)
-        pj = pj/pj[-1]
-        pt2d = np.append(pt2d, pj)
 
-    return pt2d.reshape(-1,3)
+def reproject(img, P, point4d):
+    image_points = np.dot(P, point4d)
+    image_points = image_points/image_points[-1] 
+    #print('image_points = {0}'.format(image_points))
+    x = image_points[0]
+    y = image_points[1]
+    for i in range(len(x)):
+        cv.circle(img, (int(x[i]), int(y[i])),2,(255,0,0),thickness=3)
+    cv.imwrite('keypoints2.jpg',img)
 
+def project_axis(img, P, num):
+    print('P = {0}'.format(P))
+    point4d = np.array([[0.0, 0.0, 10.0, 1.0],
+                        [1.0, 0.0, 10.0, 1.0],
+                        [0.0, 1.0, 10.0, 1.0],
+                        [0.0, 0.0, 11.0, 1.0]],dtype=np.float32)
+    image_points = np.dot(P, point4d.T)
+    image_points = image_points/image_points[-1] 
+    x = image_points[0]
+    y = image_points[1]
+    print('image_points = {0}'.format(image_points))
+    for i in range(len(x)):
+        cv.line(img,(int(x[0]), int(y[0])),(int(x[i]), int(y[i])),color=(255,0,0),thickness=9)
+    cv.imwrite('axis{0}.jpg'.format(num),img)
+
+    
--- scene.py
+++ scene.py
@@ -1,12 +1,14 @@
 import numpy as np 
 import cv2 as cv
 import matplotlib.pyplot as plt
+import open3d as o3d
 import common 
 print(cv.__version__)
+print(o3d.__version__)
 SCALE_FACTOR = 1
 focal = 3351
-img1 = cv.imread('corners/11.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-img2 = cv.imread('corners/12.jpg',cv.IMREAD_GRAYSCALE) # trainImage
+img1 = cv.imread('corners/01.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
+img2 = cv.imread('corners/02.jpg',cv.IMREAD_GRAYSCALE) # trainImage
 #initiate SIFT detector
 sift = cv.SIFT_create()
 # find the keypoints and descriptors with SIFT
@@ -45,7 +47,10 @@ E, mask = cv.findEssentialMat(pt1, pt2, cameraMatrix=cameraMatrix,method=cv.RANS
 mask = mask.flatten()
 inpt1 = np.array([pt1[i] for i in range(len(mask)) if mask[i]==1],dtype=np.float32)
 inpt2 = np.array([pt2[i] for i in range(len(mask)) if mask[i]==1],dtype=np.float32)
-#print("inpt2 = {0}".format(inpt2))
+inpt1 = np.append(inpt1, [[1811,2264]],axis=0)
+inpt2 = np.append(inpt2, [[1518,2033]],axis=0)
+print("inpt2 = {0}".format(inpt2))
+
 print("len inpt2 = {0}".format(len(inpt2)))
 points, R,t,mask = cv.recoverPose(E, pt1, pt2)
 print('recover points = {0}'.format(points))
@@ -57,24 +62,70 @@ p1 = np.array([[1.0, 0.0, 0.0,0.0],
                [0.0, 0.0, 1.0,0.0]],dtype=np.float32)
 p1 = np.dot(cameraMatrix,p1)
 #p2 = K*[R|t]
-p2 = np.append(R, np.dot(-R,t), axis=1)
+p2 = np.append(R,np.dot(-R,t), axis=1)
 p2 = np.dot(cameraMatrix,p2)
 
-print('p2 = {0}'.format(p2))
+#print('p2 = {0}'.format(p2))
 points4d = cv.triangulatePoints(p1,p2,inpt1.T,inpt2.T)
-print('points4d shape = {0}'.format(points4d.shape))
+#print('points4d shape = {0}'.format(points4d.shape))
+
 for i, e in enumerate(points4d):
     points4d[i] = e / points4d[-1]
 points3d = points4d[:-1,:].T
-print('point3d = {0}'.format(points3d[0]))
-#TODO solve pnp
-retval, rvec, tvec = cv.solvePnP(points3d,inpt2,cameraMatrix,None)
-print('PNP: {0}, {1}, {2}'.format(retval, rvec, tvec))
 
-print('project to image:')
-point2d = common.project3dpt(cameraMatrix, rvec.flatten(), tvec.flatten(), points3d)
-print(point2d[:,:-1])
-# cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3)
-plt.show()
+print('point3d[-1] = {0}'.format(points3d[-1]))
+
+## reproject points
+common.project_axis(img1, p1, 1)
+common.project_axis(img2, p2, 2)
+#TODO solve pnp
+#retval, rvec, tvec = cv.solvePnP(points3d,inpt1,cameraMatrix,None)
+#print('rvec = {0}, tvec = {1}'.format(rvec, tvec))
+#rotm, _ = cv.Rodrigues(rvec)
+extrinsic1 = np.append(p1,[[0.0, 0.0, 0.0, 1.0]],axis=0)
+extrinsic1 = [[1.0, 0.0, 0.0, 0.0],
+     [0.0, 1.0, 0.0, 0.0],
+     [0.0, 0.0, 1.0, 0.0],
+     [0.0, 0.0, 0.0, 1.0]]
+transform= [[1.0, 0.0, 0.0, 0.0],
+     [0.0, -1.0, 0.0, 0.0],
+     [0.0, 0.0, -1.0, 0.0],
+     [0.0, 0.0, 0.0, 1.0]]
+
+extrinsic2 = np.append(R, np.dot(-R,t), axis=1)
+extrinsic2 = np.append(extrinsic2, [[0.0, 0.0, 0.0, 1.0]],axis=0)
+#extrinsic2 = np.dot(extrinsic2,transform)
+#point2d = common.project3dpt(cameraMatrix, rvec.flatten(), tvec.flatten(), points3d)
+#print(point2d[:,:-1])
+#background = cv.imread('corners/1.jpg')
+#common.project_axis(background,extrinsic1)
+# load obj file and render scene
+vis = o3d.visualization.Visualizer()
+vis.create_window(width=width, height=height)
+mesh = o3d.io.read_triangle_mesh('corners/female.obj')
+mesh.compute_vertex_normals()
+mesh.translate([-0.0289, 0.1623, 9.117])
+camera = o3d.camera.PinholeCameraParameters()
+print('width = {0}, height = {1}'.format(width, height))
+intrinsic = o3d.camera.PinholeCameraIntrinsic(width, height, focal, focal, 2019.0, 1547.0)
+print('camera intrinsic = {0}'.format(intrinsic.intrinsic_matrix))
+camera.intrinsic = intrinsic
+#P = np.append(R, np.dot(-R,t),axis=1)
+
+#print('P = {0}'.format(P))
+camera.extrinsic = extrinsic1
+#print('camera extrinsic1 = {0}'.format(extrinsic1))
+# draw axis on image
+#add geometry to image
+vis.add_geometry(mesh)
+image = o3d.io.read_image('corners/01.jpg')
+
+vis.add_geometry(image)
+ctr = vis.get_view_control()
+ctr.convert_from_pinhole_camera_parameters(camera,True)
+vis.poll_events()
+vis.update_renderer()
+vis.capture_screen_image('output/out1.png', True)
+vis.run()
+#vis.remove_geometry(mesh)
+
